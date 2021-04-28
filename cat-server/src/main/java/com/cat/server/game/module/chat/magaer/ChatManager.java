cf4f3fc082d3618d492834c61a70d70da55ecec5
Jeremy <Jeremy@DESKTOP-IDPUN0O>
2021-04-29 00:03:22 +0800

    代码提交

--- cat-server/src/main/java/com/cat/server/game/module/chat/magaer/ChatManager.java
+++ cat-server/src/main/java/com/cat/server/game/module/chat/magaer/ChatManager.java
@@ -14,19 +14,6 @@ public class ChatManager implements IModuleManager<Integer, ChatDomain>{
 	/**域缓存*/
 	protected final Map<Integer, ChatDomain> domains = new ConcurrentHashMap<>();
 	
-//	/**
-//	 * 聊天缓存, 所有频道聊天缓存, 使用Cache替代掉Map
-//	 * key:BigInteger, uniqueId, 唯一id
-//	 * value:聊天记录实体bean
-//	 */
-//	private Cache<BigInteger, Chat> chatRecordMap = CacheBuilder.newBuilder()
-//			.expireAfterAccess(1, TimeUnit.HOURS)// 在给定时间内没有被读/写访问,则清除
-//			.maximumSize(1000)//	最大条目,超过这个聊天记录, 根据LRU特点移除
-//			.removalListener((notification)->{//移除监听,因超时被移除的聊天数据,持久化到数据库
-//				Chat bean = (Chat)notification.getValue();
-//				bean.delete();
-//			}).build();
-	
 	/**
 	 * 获取数据, 获取不到从数据库获取
 	 */
