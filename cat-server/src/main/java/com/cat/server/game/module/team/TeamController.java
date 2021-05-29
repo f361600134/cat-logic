4d7908232e0150b0ec928dd9f66611f0ff0dd66b
Jeremy <361600134@qq.com>
2021-05-29 13:55:32 +0800

    修改了些组队相关的代码

---
+++ cat-server/src/main/java/com/cat/server/game/module/team/TeamController.java
@@ -0,0 +1,21 @@
+package com.cat.server.game.module.team;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 队伍控制器
+ * @auth Jeremy
+ * @date 2021年5月16日下午9:14:49
+ */
+@Controller
+public class TeamController {
+
+	//创建队伍
+	//解散队伍
+	//邀请进入队伍
+	//踢出队伍
+	//查询队伍
+	//申请队伍
+	//审批申请
+	
+}
---
+++ cat-server/src/main/java/com/cat/server/game/module/team/assist/TeamConstant.java
@@ -0,0 +1,12 @@
+package com.cat.server.game.module.team.assist;
+
+import com.cat.server.utils.TimeUtil;
+
+public class TeamConstant {
+	
+	/**
+	 * 时间限制, 默认5分钟, 如果5分
+	 */
+	public static long LIMIT_TIME = TimeUtil.HOUR_MILLISECONDS;
+	
+}
