fbc965a3d2c939e8d545f6646a3723d7d68a027a
Jeremy <361600134@qq.com>
2021-12-02 18:30:57 +0800

    邮件模块整理的比较好了

---
+++ cat-server/src/main/java/com/cat/server/game/module/mail/event/MailObserver.java
@@ -0,0 +1,40 @@
+package com.cat.server.game.module.mail.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cat.server.core.event.IObserver;
+import com.cat.server.game.module.mail.MailService;
+import com.cat.server.game.module.player.event.PlayerLeaveGameEvent;
+import com.cat.server.game.module.player.event.PlayerLoginEndEvent;
+import com.google.common.eventbus.Subscribe;
+
+/**
+ * PlayerMail事件类
+ */
+@Component
+public class MailObserver implements IObserver{
+
+	@Autowired
+    private MailService mailService;
+	
+	 /**
+     * 登录事件
+     * @param event
+     */
+    @Subscribe
+    public void onPlayerAfterLoginEvent(PlayerLoginEndEvent event) {
+    	mailService.onLogin(event.getPlayerId());
+    }
+    
+//    /**
+//     * 离线事件
+//     * @param event
+//     */
+//    @Subscribe
+//    public void onPlayerLeaveGameEvent(PlayerLeaveGameEvent event) {
+//    	mailService.onLogout(event.getPlayerId());
+//    }
+
+
+}
